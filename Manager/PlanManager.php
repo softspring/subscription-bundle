8588d25155f131384635fd1602d45ee31d312055
Javi H. Gil <javihgil@gmail.com>
2020-06-11 14:12:45 +0200

    Update PlanManager sync action to support platform integration

--- Manager/PlanManager.php
+++ Manager/PlanManager.php
@@ -2,8 +2,14 @@
 
 namespace Softspring\SubscriptionBundle\Manager;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\EntityManagerInterface;
 use Softspring\CrudlBundle\Manager\CrudlEntityManagerTrait;
+use Softspring\PlatformBundle\Adapter\PlanAdapterInterface;
+use Softspring\PlatformBundle\Manager\AdapterManagerInterface;
+use Softspring\PlatformBundle\Model\PlatformObjectInterface;
+use Softspring\PlatformBundle\Provider\PlatformProviderInterface;
 use Softspring\SubscriptionBundle\Model\PlanInterface;
 
 class PlanManager implements PlanManagerInterface
@@ -21,6 +27,16 @@ class PlanManager implements PlanManagerInterface
     protected $productManager;
 
     /**
+     * @var PlatformProviderInterface|null
+     */
+    protected $platformProvider;
+
+    /**
+     * @var AdapterManagerInterface|null
+     */
+    protected $adapterManager;
+
+    /**
      * PlanManager constructor.
      *
      * @param EntityManagerInterface       $em
@@ -30,6 +46,22 @@ class PlanManager implements PlanManagerInterface
     {
         $this->em = $em;
         $this->productManager = $productManager;
+    }
+
+    /**
+     * @param AdapterManagerInterface|null $adapterManager
+     */
+    public function setAdapterManager(?AdapterManagerInterface $adapterManager): void
+    {
+        $this->adapterManager = $adapterManager;
+    }
+
+    /**
+     * @param PlatformProviderInterface|null $platformProvider
+     */
+    public function setPlatformProvider(?PlatformProviderInterface $platformProvider): void
+    {
+        $this->platformProvider = $platformProvider;
     }
 
     public function getTargetClass(): string
@@ -58,50 +90,53 @@ class PlanManager implements PlanManagerInterface
      */
     public function syncAll(): void
     {
-//        $dbPlans = new ArrayCollection($this->getRepository()->findAll());
-//        // $platformPlans = $this->api->get('plan')->list();
-//
-//        /** @var PlanResponse $platformPlan */
-//        foreach ($platformPlans as $platformPlan) {
-//            $dbPlan = $dbPlans->filter(function(PlanInterface $plan) use ($platformPlan) {
-//                return $plan->getPlatformId() == $platformPlan->getId();
-//            })->first();
-//
-//            if (!$dbPlan) {
-//                // not found, create in db
-//                $dbPlan = $this->createEntity();
-//                // $dbPlan->setPlatform($this->api->platformId());
-//                $dbPlan->setPlatformId($platformPlan->getId());
-//            }
-//
-//            $dbPlan->setName($platformPlan->getName());
-//            $dbPlan->setActive($platformPlan->getActive());
-//            $dbPlan->setAmount($platformPlan->getAmount());
-//            $dbPlan->setCurrency($platformPlan->getCurrency());
-//            $dbPlan->setTestMode($platformPlan->isTesting());
-//            $dbPlan->setInterval($platformPlan->getInterval());
-//            $dbPlan->setPlatformData($platformPlan->getPlatformNativeArray());
-//            $dbPlan->setPlatformLastSync(new \DateTime('now'));
-//            $dbPlan->setPlatformConflict(false);
-//
-//            if ($dbPlan instanceof PlanHasProductInterface) {
-//                if ($this->productManager && $platformPlan->getProductId()) {
-//                    $dbProduct = $this->productManager->getRepository()->findOneByPlatformId($platformPlan->getProductId());
-//                    $dbPlan->setProduct($dbProduct);
-//                } else {
-//                    $dbPlan->setProduct(null);
-//                }
-//            }
-//
-//            $this->saveEntity($dbPlan);
-//
-//            $dbPlans->removeElement($dbPlan);
-//        }
-//
-//        /** @var PlanInterface $dbPlan */
-//        foreach ($dbPlans as $dbPlan) {
-//            $dbPlan->setPlatformConflict(true);
-//            $this->saveEntity($dbPlan);
-//        }
+        if (!$this->adapterManager) {
+            return;
+        }
+
+        $dbPlans = $this->getRepository()->findAll();
+
+        if (!$dbPlans instanceof Collection) {
+            $dbPlans = new ArrayCollection($dbPlans);
+        }
+
+        $platformProvider = $this->platformProvider;
+
+        /** @var PlanAdapterInterface $adapter */
+        foreach ($this->adapterManager->getByType('plan') as $platform => $adapter) {
+            $platformPlans = $adapter->list();
+            $transformer = $adapter->getTransformer();
+
+            foreach ($platformPlans as $platformPlan) {
+                $dbPlan = $dbPlans->filter(function(PlanInterface $plan) use ($platform, $platformProvider, $platformPlan) {
+                    /** @var PlatformObjectInterface $plan */
+
+                    if ($platformPlan instanceof \Stripe\Plan) {
+                        return $plan->getPlatformId() == $platformPlan->id && $platformProvider->getPlatform($plan) == $platform;
+                    }
+
+                    // not yet supported
+                    return false;
+                })->first();
+
+                if (!$dbPlan) {
+                    $dbPlan = $this->createEntity();
+                }
+
+                $transformer->reverseTransform($platformPlan, $dbPlan);
+
+                $dbPlan->setPlatformWebhooked(true); // skip platform processing
+
+                $this->saveEntity($dbPlan);
+
+                $dbPlans->removeElement($dbPlan);
+            }
+        }
+
+        /** @var PlanInterface|PlatformObjectInterface $dbPlan */
+        foreach ($dbPlans as $dbPlan) {
+            $dbPlan->setPlatformConflict(true);
+            $this->saveEntity($dbPlan);
+        }
     }
 }
